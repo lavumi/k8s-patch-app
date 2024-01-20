3b449211f1b046e094c6492b42a156b9593d3675
Lavumi <35640315+lavumi@users.noreply.github.com>
2024-01-20 09:37:56 +0900

    add get, set, patch functions

---
+++ src/k8s_client.rs
@@ -0,0 +1,101 @@
+
+use k8s_openapi::api::apps::v1::{Deployment};
+use kube::{Api, Client, Error};
+use kube::api::{Patch, PatchParams};
+use serde_json::{json, Value};
+use json_patch;
+
+pub struct K8sClient{}
+
+
+impl K8sClient{
+    async fn get_client(&self) -> Result<Client, Error> {
+        let client = Client::try_default().await?;
+        Ok(client)
+    }
+
+    pub async fn get_replicas(&self,namespace: Option<&String>, deployment_name : &String)->Result<i32, Error>{
+        let client = self.get_client().await?;
+        let deployments:Api<Deployment> = Api::default_namespaced(client);
+        let deployment = deployments.get(deployment_name).await?;
+        if let Some(spec) = deployment.spec {
+            if let Some(replicas) = spec.replicas{
+                return Ok(replicas);
+            }
+        };
+        //이거 에러 데체 어케 내뿜는거지
+        return Ok(-1);
+    }
+
+    pub async fn set_replicas(&self,namespace: Option<&String>, deployment_name : &String, count : i32)->Result<(), Error>{
+        let client = self.get_client().await?;
+        let deployments:Api<Deployment> = Api::default_namespaced(client);
+        let patches = json!({
+            "spec": {
+                "replicas": count
+            }
+        });
+        let params = PatchParams::apply("k9s-patch-app");
+        deployments.patch(&deployment_name, &params, &Patch::Merge(patches)).await?;
+        Ok(())
+    }
+
+    pub async fn patch_params(&self, namespace: Option<&String>, deployment_name : &String, patches :json_patch::Patch)-> Result<(), Error>{
+        let client = self.get_client().await?;
+        let deployments:Api<Deployment> = Api::default_namespaced(client);
+        let params = PatchParams::apply("k9s-patch-app");
+        deployments.patch(&deployment_name, &params, &Patch::<Value>::Json(patches)).await?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use kube::Error;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get(){
+        let c = K8sClient{};
+        let deployment =  "my-webapp".to_string();
+        let res = c.get_replicas(None, &deployment ).await.expect("get replicas fail");
+        print!("success  count:{}" , res)
+    }
+
+    #[tokio::test]
+    async fn test_set(){
+        let c = K8sClient{};
+        let deployment =  "my-webapp".to_string();
+        c.set_replicas(None, &deployment, 3).await.expect("set test fail");
+    }
+
+    #[tokio::test]
+    async fn test_set_unknown_deployment(){
+        let c = K8sClient{};
+        let deployment =  "my-webapp2".to_string();
+        let res = c.set_replicas(None, &deployment, 3).await;
+        if let Err(e_) = res {
+            print!("my-webapp2 not exist - success");
+        }
+        else {
+            panic!("my-webapp2 not exist - fail");
+        }
+    }
+
+
+    #[tokio::test]
+    async fn test_patch(){
+
+        let namespace = None;
+        let deployment =  "my-webapp".to_string();
+        let patches = r#"[{"op": "replace", "path": "/spec/replicas", "value": 2}]"#;
+        let patches :json_patch::Patch= match serde_json::from_str(&patches) {
+            Ok(p) => p,
+            Err(e) => {
+                panic!("{}", e)
+            }
+        };
+        let c = K8sClient{};
+        c.patch_params(namespace, &deployment, patches).await.expect("TODO: panic message");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,85 @@
-use clap::{arg, command, ArgAction, Command};
-use k8s_openapi::api::apps::v1::Deployment;
-use kube::{Api, Client};
-use kube::api::{Patch, PatchParams};
-use serde_json::{Value};
+mod k8s_client;
+
+use clap::{arg, command, Command, ArgMatches};
 use tokio;
+use crate::k8s_client::K8sClient;
+
+fn make_commands()-> ArgMatches{
+    command!()
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("get")
+                .about("Get the Current Number of Replicas in a Deployment")
+                .arg(
+                    arg!(-n --namespace <NAMESPACE> "namespace or default"))
+                .arg(
+                    arg!(-d --deployment <DEPLOYMENT> "target deployment name")
+                        .required(true))
+        )
+        .subcommand(
+            Command::new("set")
+                .about("Set the Number of Replicas in a Deployment")
+                .arg(
+                    arg!(-n --namespace <NAMESPACE> "namespace or default"))
+                .arg(
+                    arg!(-d --deployment <DEPLOYMENT> "target deployment name")
+                        .required(true))
+                .arg(
+                    arg!(-c --count <COUNT> "number of replicas you want")
+                        .required(true))
+        )
+        .subcommand(
+            Command::new("patch")
+                .about("Patch to a Deployment")
+                .arg(
+                    arg!(-n --namespace <NAMESPACE> "namespace or default"))
+                .arg(
+                    arg!(-d --deployment <DEPLOYMENT> "target deployment name")
+                        .required(true))
+                .arg(
+                    arg!(-p --patch <PATCH_JSON> "json to patch")
+                        .required(true))
+        )
+        .get_matches()
+}
 
 #[tokio::main]
 async fn main() -> Result<(), kube::Error> {
-    let matches = command!()
-        .subcommand(
-            Command::new("get")
-                .about("")
-                .arg(arg!(-l --list "lists test values").action(ArgAction::SetTrue)),
-        )
-        .arg(
-            arg!(-n --name <DEPLOYMENT> "target deployment name")
-                .required(true))
-        .arg(
-            arg!(-p --patch <JSON> "k8s json patch")
-            .required(true))
-        .get_matches();
-
-    let name = matches.get_one::<String>("name").cloned().unwrap();
-    let patches_str = matches.get_one::<String>("patch").cloned().unwrap();
-
-    let patches = serde_json::from_str(&patches_str).unwrap();
-
-    let client = Client::try_default().await?;
-    let deployments:Api<Deployment> = Api::default_namespaced(client);
-    let params = PatchParams::apply("k9s-patch-app");
-    deployments.patch(&name, &params, &Patch::<Value>::Json(patches)).await?;
-
-    Ok(())
+    let matches = make_commands();
+    let result = match matches.subcommand(){
+        Some(("get",sub_matches))=>{
+            let namespace = sub_matches.get_one::<String>("namespace");
+            let deployment = sub_matches.get_one::<String>("deployment").unwrap();
+            let k8s_client = K8sClient{};
+            let replicas = k8s_client.get_replicas(namespace, deployment).await?;
+            print!("replica count : {}\n" , replicas);
+        }
+        Some(("set",sub_matches))=>{
+            let namespace = sub_matches.get_one::<String>("namespace");
+            let deployment = sub_matches.get_one::<String>("deployment").unwrap();
+            let count = sub_matches.get_one::<String>("count").unwrap().parse::<i32>().expect("fail to get count");
+            let k8s_client = K8sClient{};
+            k8s_client.set_replicas(namespace, deployment, count).await?;
+        }
+        Some(("patch",sub_matches))=>{
+            let namespace = sub_matches.get_one::<String>("namespace");
+            let deployment = sub_matches.get_one::<String>("deployment").unwrap();
+            let patches_str = sub_matches.get_one::<String>("patch").unwrap();
+            let patches :json_patch::Patch= match serde_json::from_str(&patches_str) {
+                Ok(p) => p,
+                Err(e) => {
+                    panic!("{}", e)
+                }
+            };
+            let k8s_client = K8sClient{};
+            k8s_client.patch_params(namespace, deployment, patches).await.expect("patch fail");
+        }
+        Some(_) => {
+            panic!("command needed")
+        }
+        None => {panic!("command needed")}
+    };
+    Ok(result)
 }
+
